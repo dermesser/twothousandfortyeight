cfbfe635339a76d9d26a6fcc065492c23e2bd279
lbo <lbo@spheniscida.de>
2014-05-19 21:45:29 +0200

    Little cleanup

--- Twothousandfourtyeight.hs
+++ Twothousandfourtyeight.hs
@@ -27,11 +27,11 @@ shiftField ToLeft f = absoluteMap $ foldl' (moveTile ToLeft) f [(r,c) | r <- [1.
 shiftField ToRight f = absoluteMap $ foldr (flip $ moveTile ToRight) f [(r,c) | r <- [1..sidelength], c <- [1..sidelength-1]]
 
 moveTile :: Direction -> Field -> (Int,Int) -> Field
-moveTile d f p@(r,c) = case f ! p of
+moveTile d f p = case f ! p of
                             0 -> f
                             v | not (isVoidAddress nextfield) -> case f ! nextfield of
                                     0 -> moveTile d (multipleSet [(nextfield,f ! p),(p,0)] f) nextfield
-                                    nextv | nextv == v && not (isVoidAddress nextfield) -> multipleSet [(nextfield,-2 * (f ! p)),(p,0)] f
+                                    nextv | nextv == v -> multipleSet [(nextfield,-2 * (f ! p)),(p,0)] f
                                           | otherwise -> f
                               | otherwise -> f
     where nextfield = advanceField d p
@@ -42,9 +42,6 @@ advanceField ToDown (r,c) = (r+1,c)
 advanceField ToLeft (r,c) = (r,c-1)
 advanceField ToRight (r,c) = (r,c+1)
 
-
-isVoidAddress :: (Int,Int) -> Bool
-isVoidAddress (r,c) = r < 1 || c < 1 || r > sidelength || c > sidelength
 
 -- Random generation of tiles.
 
@@ -59,9 +56,9 @@ getTile = do
 getFreePosition :: Field -> IO (Int,Int)
 getFreePosition f = do
     i <- randomIO
-    if length freepos > 0
-    then return (freepos !! abs (i `mod` length freepos))
-    else return (0,0)
+    return (if not $ null freepos
+            then freepos !! abs (i `mod` length freepos)
+            else (0,0))
     where freepos = getZeroes f
 
 placeRandomTile :: Field -> IO Field
@@ -81,6 +78,9 @@ multipleSet l f = foldr (\(p,v) oldfield -> setElem v p oldfield) f l
 
 getZeroes :: Field -> [(Int,Int)]
 getZeroes f = foldr (\p c -> if (f ! p) /= 0 then c else p:c) [] [(r,c) | r <- [1..sidelength], c <- [1..sidelength]]
+
+isVoidAddress :: (Int,Int) -> Bool
+isVoidAddress (r,c) = r < 1 || c < 1 || r > sidelength || c > sidelength
 
 matrixMap :: (a -> a) -> Matrix a -> Matrix a
 matrixMap f m = foldr (\p mat -> setElem (f (mat ! p)) p mat) m [(r,c) | r <- [1..nrows m], c <- [1..ncols m]]
